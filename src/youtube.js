3aa9ca0003bea7a2bb28682516b306b93730f78a
uzimith <uzimith.x9@gmail.com>
2015-11-29 10:51:42 +0900

    move script to javascript

--- src/youtube.js
+++ src/youtube.js
@@ -1,5 +1,11 @@
 var player;
 
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
 function onYouTubeIframeAPIReady() {
   player = new YT.Player('player', {
     height: '390',
